e08c883defb8a7b150eedc4f212c03b1b5af2dbd
JoseAl05 <jparancibialinker@gmail.com>
2023-04-07 19:23:18 -0400

    create register modal and a hook to handle it

---
+++ app/components/modals/RegisterModal.tsx
@@ -0,0 +1,24 @@
+'use client';
+
+import axios from 'axios';
+import { useCallback, useState } from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+import { FcGoogle } from 'react-icons/fc';
+import {
+    FieldValues,
+    SubmitHandler,
+    useForm,
+} from 'react-hook-form'
+
+import useRegisterModal from '@/app/hooks/useRegisterModal';
+
+
+const RegisterModal = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default RegisterModal;
