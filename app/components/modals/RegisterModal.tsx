0b7cb43c6a87fce0c6ba4fd292fc03b46dc2ecf6
JoseAl05 <jparancibialinker@gmail.com>
2023-04-07 19:59:32 -0400

    add modal header and input component to register modal

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -11,13 +11,65 @@ import {
 } from 'react-hook-form'
 
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import Modal from './Modal';
+import ModalHeader from './ModalHeader';
+import Input from '../inputs/Input';
 
 
 const RegisterModal = () => {
-    return (
-        <div>
-            Enter
+    const registerModal = useRegisterModal();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const {
+        register,
+        handleSubmit,
+        formState: {
+            errors
+        },
+    } = useForm<FieldValues>({
+        defaultValues: {
+            name: '',
+            email: '',
+            password: ''
+        }
+    });
+
+    const onSubmit: SubmitHandler<FieldValues> = (data) => {
+        setIsLoading(true);
+
+        //Axios POST call to register end point
+        axios.post('/api/signup', data)
+            .then(() => {
+                registerModal.onClose();
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            })
+    }
+
+    const bodyContent = (
+        <div className='flex flex-col gap-4'>
+            <ModalHeader title='Welcome to Airbnb' subtitle='Create your account' />
+            <Input id='email' label='Email' disabled={isLoading} register={register} errors={errors} required />
+            <Input id='name' label='Name' disabled={isLoading} register={register} errors={errors} required />
+            <Input id='password' label='Password' type='password' disabled={isLoading} register={register} errors={errors} required />
         </div>
+    )
+
+
+    return (
+        <Modal
+            disabled={isLoading}
+            isOpen={registerModal.isOpen}
+            title='Sign Up'
+            actionLabel='Continue'
+            onClose={registerModal.onClose}
+            onSubmit={handleSubmit(onSubmit)}
+            body={bodyContent}
+        />
     );
 }
 
