b8ed593cf5e2692af3e6048b906273ff01ea03d6
JoseAl05 <jparancibialinker@gmail.com>
2023-05-03 23:43:09 -0400

    fix logo size

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -9,7 +9,7 @@ const Logo = () => {
     const router = useRouter();
 
     return (
-        <Image onClick={() => router.push('/')} alt='logo' className='hidden cursor-pointer md:block' height='50' width='50' src='/images/logo.png' />
+        <Image onClick={() => router.push('/')} alt='logo' className='hidden cursor-pointer md:block' height='125' width='125' src='/images/logo.png' />
     );
 }
 
