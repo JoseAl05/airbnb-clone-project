c05409ffa13321305b5020e6fbc371152dd18cb0
JoseAl05 <jparancibialinker@gmail.com>
2023-05-04 19:41:21 -0400

    add redirection to reservetions in user menu

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -97,7 +97,7 @@ const UserMenu: React.FC<UserMenuProps> = ({ currentUser }) => {
                                     label='My favorites'
                                 />
                                 <MenuItem
-                                    onClick={() => { }}
+                                    onClick={() => router.push('/reservations')}
                                     label='My reservations'
                                 />
                                 <MenuItem
