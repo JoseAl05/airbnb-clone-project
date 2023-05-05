becfe96f03624946df4287d6dd1bef66621a6350
JoseAl05 <jparancibialinker@gmail.com>
2023-05-05 18:40:21 -0400

    create favorites page

---
+++ app/favorites/page.tsx
@@ -0,0 +1,35 @@
+import EmptyState from '../components/EmptyState';
+import ClientOnly from '../components/ClientOnly';
+
+import getCurrentUser from '../functions/getCurrentUser';
+import getFavoriteListings from '../functions/getFavoriteListings';
+import FavoritesClient from '../components/favorites/FavoritesClient';
+
+const FavoritesPage = async() => {
+
+    const favoritesListings = await getFavoriteListings();
+    const currentUser = await getCurrentUser();
+
+    if(favoritesListings.length === 0){
+        return (
+            <ClientOnly>
+                <EmptyState
+                    title='No favorites found'
+                    subtitle='Looks like you have no favorite listings'
+                />
+            </ClientOnly>
+        );
+    }
+
+    return (
+        <ClientOnly>
+            <FavoritesClient
+                favoritesListings={favoritesListings}
+                currentUser={currentUser}
+            />
+        </ClientOnly>
+    )
+
+}
+
+export default FavoritesPage;
